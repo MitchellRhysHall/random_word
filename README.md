0f39a6b2d87d62543c4fac46fa5f2cd605921235
Mitchell Rhys Hall <Mitchell.Rhys.Hall@protonmail.com>
2022-10-31 20:25:23 +1100

    updated functions to reference static word array

--- README.md
+++ README.md
@@ -3,11 +3,9 @@
 The `random_word` crate provides a simple way to generate random 
 english words.
 
-The `random_word::Words` associated functions are asynchronous calls to a third party api.
-
 ## Generating a random word
 
 ```rust
-use random_word::Words;
-let word = Words::random().await;
+use random_word;
+let word = generate();
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -5,143 +5,80 @@
 //!
 //! The `random_word` crate provides a simple way to generate random 
 //! english words.
-//!
-//! The [`random_word`][Words] associated functions are asynchronous
-//! calls to a third party api.
+//! 
+//! All 178,187 words are compiled with the library.
 //! 
 //! ## Generating a random word
 //!
 //! ```rust
-//! use random_word::Words;
-//! # async fn random_word() -> Result<(), random_word::Error> {
-//! let word = Words::random().await;
+//! use random_word;
+//! # fn generate() -> Result<(),&'static str> {
+//! let word = generate();
 //! # Ok(())
 //! # }
 //! ```
 
-static SINGLE_WORD_URL: &str = "https://random-word-api.herokuapp.com/word";
-static ALL_WORD_URL: &str = "https://random-word-api.herokuapp.com/all";
+use rand::random;
+mod words;
+use words::WORDS;
 
-
-/// A `Result` alias where the `Err` case is `random_word::Error`.
-pub type Result<T> = std::result::Result<T, Error>;
-
-/// The Errors that may occur.
+/// Returns a reference to a randomly generated english word.
 ///
-/// Note: This is an enumumation of dependency crate errors
-#[derive(Debug)]
-pub enum Error {
-    /// All networking errors which can occur.
-    Network(reqwest::Error),
-    /// All json parsing errors which can occur.
-    ParseJson(serde_json::Error)
+/// # Examples
+///
+/// ```rust
+/// use random_word;
+/// # fn generate() -> Result<(),&'static str> {
+/// let word = generate();
+/// # Ok(())
+/// # }
+/// ```
+///
+pub fn generate() -> &'static str {
+    WORDS[random::<usize>() % WORDS.len()]
 }
 
-
-/// An empty struct for calling associated functions.
-#[derive(Debug)]
-pub struct Words;
-
-impl Words {
-    /// Generates a random english word.
-    /// 
-    /// **NOTE**: This function makes a http request.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use random_word::Words;
-    /// # async fn random_word() -> Result<(), random_word::Error> {
-    /// let word = Words::random().await;
-    /// # Ok(())
-    /// # }
-    /// ```
-    ///
-    /// # Errors
-    ///
-    /// This function fails if:
-    ///
-    /// - there is an error fetching data from the third party provider
-    pub async fn random() -> Result<String> {
-        fetch(SINGLE_WORD_URL).await
-    }
-
-    /// Generates all known english words sorted alphabetically.
-    /// 
-    /// **NOTE**: This function makes a http request.
-    ///
-    /// **WARNING**: This is a large dataset (>2MB).
-    /// 
-    /// # Examples
-    ///
-    /// ```rust
-    /// use random_word::Words;
-    /// # async fn all_words() -> Result<(), random_word::Error> {
-    /// let all_words = Words::all().await;
-    /// # Ok(())
-    /// # }
-    /// ```
-    ///
-    /// # Errors
-    ///
-    /// This function fails if:
-    ///
-    /// - there is an error fetching data from the third party provider
-    /// - the data received from the third party provider is malformed
-    pub async fn all() -> Result<Vec<String>> {
-        fetch(ALL_WORD_URL).await
-            .and_then(|s| from_json::<Vec<String>>(&s))
-    }
+/// Test function for 'generate' method
+#[test]
+fn generate_test() {
+    let mut word_list = vec![];
+    (0..50).for_each(|_| {
+        let word = generate();
+        assert!(!word_list.contains(&word), 
+            "List of up to 50 previously generated words contains duplicates.");
+        assert!(!word.is_empty(), "generated word should not be empty");
+        word_list.push(word);
+    })
 }
 
-async fn fetch(url: &str) -> Result<String> {
-    match reqwest::get(url).await {
-        Ok(response) => match response.text().await {
-            Ok(data) => Ok(data),
-            Err(error) => Err(Error::Network(error))
-        },
-        Err(error) => Err(Error::Network(error))
-    }
+/// Returns a reference to 178,187 english words.
+///
+/// # Examples
+///
+/// ```rust
+/// use random_word;
+/// # fn generate_all() -> Result<(),[&'static str; 178187]> {
+/// let word_list = generate_all();
+/// # Ok(())
+/// # }
+/// ```
+///
+pub fn generate_all() -> &'static [&'static str; 178187] {
+    &WORDS
 }
 
-fn from_json<T>(json: &str) -> Result<T>
-    where T: serde::de::DeserializeOwned {
-    match serde_json::from_str::<T>(json) {
-        Ok(data_struct) => Ok(data_struct),
-        Err(error) => Err(Error::ParseJson(error))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn random_word() {
-        let future1 = Words::random();
-        let future2 = Words::random();
-        match [future1.await, future2.await] {
-            [Ok(word1), Ok(word2)] => {
-                assert_ne!(word1,word2);
-                assert!([word1,word2].iter().all(|word| {
-                   !word.is_empty() &&
-                    word.chars().all(|c| !('0'..='9').contains(&c))
-                }))
-            },
-            [Err(error),_] | [_,Err(error)]  => panic!("{:#?}", error),
-        }
-    }
-
-    #[tokio::test(flavor = "multi_thread")]
-    async fn all_words() {
-        match Words::all().await {
-            Ok(all_words) => {
-                assert!(!all_words.is_empty());
-                assert!(all_words.iter().all(|w| w.chars()
-                    .all(|c| !('0'..='9')
-                        .contains(&c) && ' ' != c)));
-            },
-            Err(error) => panic!("{:#?}", error)
-        }
-    }
+/// Test function for 'generate all' method
+#[test]
+fn generate_all_test() {
+    let word_list = generate_all();
+    // let word_hash_set = word_list.iter()
+    //     .collect::<std::collections::HashSet<_>>();
+    // assert_eq!(word_list.len(), word_hash_set.len(), 
+    //     "There should be no duplicates words");
+    word_list.iter().for_each(|word| {
+        assert!(!word.is_empty(), 
+            "word should not be empty in word list");
+        assert!(word.chars().all(|c| ('a'..='z').contains(&c)),
+            "word should only contain lowercase alphabetical characters");
+    })
 }
