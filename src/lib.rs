d3156c69a6112d67f26e5672cb4951cd36ed0c64
Mitchell Rhys Hall <Mitchell.Rhys.Hall@protonmail.com>
2022-11-01 18:39:48 +1100

    moved tests to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![no_std]
 #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 
@@ -22,29 +23,19 @@
 //! ```
 
 mod words;
+mod tests;
 use words::WORDS;
 
-fn select_random(array: &'static [&'static str]) -> &'static str {
-    array[rand::random::<usize>() % array.len()]
+/// Represents all possible errors.
+#[derive(Debug)]
+pub enum Error {
+    /// Occurs when an invalid char parameter is passed to a function.
+    NonAlphabeticCharacter(char),
 }
 
-#[test]
-fn select_random_test() {
-    let array = &["1","2","3","4","5"];
-    // All options from array should be pushed into this set.
-    // This ensures that the random selection function
-    // is capable of selecting all available items from array.
-    let mut set = std::collections::HashSet::new();
-    (0..500000).for_each(|_| {
-        let choice = select_random(array);
-        set.insert(choice);
-        assert!(!choice.is_empty(), "Random choice should not be empty");
-        assert!(array.contains(&choice), "Random choice should be from input array");
-    });
-    assert_eq!(set.len(), array.len(), "Set length should match array length");
-    array.iter().for_each(|item| {
-        assert!(set.contains(item), "Set should contain array item");
-    });
+#[inline]
+fn select_random(array: &'static [&'static str]) -> &'static str {
+    array[rand::random::<usize>() % array.len()]
 }
 
 /// Returns a reference to a randomly generated english word.
@@ -58,20 +49,9 @@ fn select_random_test() {
 /// # }
 /// ```
 ///
+#[inline]
 pub fn gen() -> &'static str {
     select_random(&WORDS)
-}
-
-#[test]
-fn gen_test() {
-    let mut word_list = vec![];
-    (0..50).for_each(|_| {
-        let word = gen();
-        assert!(!word_list.contains(&word), 
-            "List of up to 50 previously generated words contains duplicates.");
-        assert!(!word.is_empty(), "generated word should not be empty");
-        word_list.push(word);
-    })
 }
 
 /// Returns an reference to a word that begins with the 
@@ -86,22 +66,12 @@ fn gen_test() {
 /// # }
 /// ```
 ///
-pub fn gen_starts_with(character: char) -> &'static str {
-    select_random(&gen_all_starts_with(character))
+#[inline]
+pub fn gen_starts_with(character: char) -> Result<&'static str, Error> {
+    Ok(select_random(&gen_all_starts_with(character)?))
 }
 
-#[test]
-fn gen_starts_with_test() {
-    ('a'..='z').for_each(|character| {
-        (0..5000).for_each(|_|{
-            let word = gen_starts_with(character);
-            assert!(!word.is_empty(), "word should not be empty");
-            assert!(word.starts_with(character), "word should start with {character}");
-        })
-    })
-}
-
-/// Returns an alphabetically ordered slice to all words 
+/// Returns an alphabetically ordered slice of all words 
 /// that begin with the specified character.
 ///
 /// # Example
@@ -113,48 +83,37 @@ fn gen_starts_with_test() {
 /// # }
 /// ```
 ///
-pub fn gen_all_starts_with(character: char) -> &'static [&'static str] {
-    &WORDS[match character {
-        'a' | 'A' =>      0..10363,
-        'b' | 'B' =>  10363..20327,
-        'c' | 'C' =>  20327..36906,
-        'd' | 'D' =>  36906..47297,
-        'e' | 'E' =>  47297..54481,
-        'f' | 'F' =>  54481..61608,
-        'g' | 'G' =>  61608..67478,
-        'h' | 'H' =>  67478..73971,
-        'i' | 'I' =>  73971..80599,
-        'j' | 'J' =>  80599..82077,
-        'k' | 'K' =>  82077..83932,
-        'l' | 'L' =>  83932..89246,
-        'm' | 'M' =>  89246..99191,
-        'n' | 'N' =>  99191..103643,
-        'o' | 'O' => 103643..109700,
-        'p' | 'P' => 109700..124756,
-        'q' | 'Q' => 124756..125605,
-        'r' | 'R' => 125605..136123,
-        's' | 'S' => 136123..155842,
-        't' | 'T' => 155842..164837,
-        'u' | 'U' => 164837..170068,
-        'v' | 'V' => 170068..172926,
-        'w' | 'W' => 172926..176844,
-        'x' | 'X' => 176844..176996,
-        'y' | 'Y' => 176996..177586,
-        'z' | 'Z' => 177586..178187,
-         _  => panic!("character is not alphabetic!")
-    }]
-}
-
-#[test]
-fn gen_all_starts_with_test() {
-    ('a'..='z').chain('A'..'Z').for_each(|character| {
-        let word_list = gen_all_starts_with(character);
-        word_list.iter().for_each(|word| {
-            let character = character.to_ascii_lowercase();
-            assert!(!word.is_empty(), "word should not be empty");
-            assert!(word.starts_with(character), "word should start with {character}");
-        })
-    })
+#[inline]
+pub fn gen_all_starts_with(character: char) -> Result<&'static [&'static str], Error> {
+    match character {
+        'a' | 'A' =>      Ok(&WORDS[0..10363]),
+        'b' | 'B' =>  Ok(&WORDS[10363..20327]),
+        'c' | 'C' =>  Ok(&WORDS[20327..36906]),
+        'd' | 'D' =>  Ok(&WORDS[36906..47297]),
+        'e' | 'E' =>  Ok(&WORDS[47297..54481]),
+        'f' | 'F' =>  Ok(&WORDS[54481..61608]),
+        'g' | 'G' =>  Ok(&WORDS[61608..67478]),
+        'h' | 'H' =>  Ok(&WORDS[67478..73971]),
+        'i' | 'I' =>  Ok(&WORDS[73971..80599]),
+        'j' | 'J' =>  Ok(&WORDS[80599..82077]),
+        'k' | 'K' =>  Ok(&WORDS[82077..83932]),
+        'l' | 'L' =>  Ok(&WORDS[83932..89246]),
+        'm' | 'M' =>  Ok(&WORDS[89246..99191]),
+        'n' | 'N' =>  Ok(&WORDS[99191..103643]),
+        'o' | 'O' => Ok(&WORDS[103643..109700]),
+        'p' | 'P' => Ok(&WORDS[109700..124756]),
+        'q' | 'Q' => Ok(&WORDS[124756..125605]),
+        'r' | 'R' => Ok(&WORDS[125605..136123]),
+        's' | 'S' => Ok(&WORDS[136123..155842]),
+        't' | 'T' => Ok(&WORDS[155842..164837]),
+        'u' | 'U' => Ok(&WORDS[164837..170068]),
+        'v' | 'V' => Ok(&WORDS[170068..172926]),
+        'w' | 'W' => Ok(&WORDS[172926..176844]),
+        'x' | 'X' => Ok(&WORDS[176844..176996]),
+        'y' | 'Y' => Ok(&WORDS[176996..177586]),
+        'z' | 'Z' => Ok(&WORDS[177586..178187]),
+         c  => Err(Error::NonAlphabeticCharacter(c))
+    }
 }
 
 /// Returns a reference to 178,187 english words in alphabetical order.
@@ -168,28 +127,7 @@ fn gen_all_starts_with_test() {
 /// # }
 /// ```
 ///
+#[inline]
 pub fn gen_all() -> &'static [&'static str; 178187] {
     &WORDS
-}
-
-#[test]
-fn gen_all_test() {
-    let word_list = gen_all();
-    word_list.iter().for_each(|word| {
-        assert!(!word.is_empty(), 
-            "word should not be empty in word list");
-        assert!(word.chars().all(|c| ('a'..='z').contains(&c)),
-            "word should only contain lowercase alphabetical characters");
-    })
-}
-
-#[test]
-fn gen_all_indexes_from_first_char() {
-    let word_list = gen_all();
-    ('a'..='z').for_each(|c| {
-        let i = word_list.iter().position(|w| w.starts_with(c))
-            .expect("words starting with any alphabetic character 
-                should be represented in words array");
-        //println!("first index starting with {c} is {i}");
-    });
 }
---
+++ src/tests.rs
@@ -0,0 +1,88 @@
+#[cfg(test)]
+
+use super::*;
+
+#[test]
+fn select_random_test() {
+    let array = &["1","2","3","4","5"];
+    // All options from array should be pushed into this set.
+    // This ensures that the random selection function
+    // is capable of selecting all available items from array.
+    let mut set = ["0"; 5];
+    (0..500000).for_each(|_| {
+        let choice = select_random(array);
+        set[match choice {
+            "1" => 0,
+            "2" => 1,
+            "3" => 2,
+            "4" => 3,
+            "5" => 4,
+             _  => panic!("Random choice is an unexpected value")
+        }] = choice;
+        assert!(!choice.is_empty(), "Random choice should not be empty");
+        assert!(array.contains(&choice), "Random choice should be from input array");
+    });
+    assert_eq!(set.len(), array.len(), "Set length should match array length");
+    array.iter().for_each(|item| {
+        assert!(set.contains(item), "Set should contain array item");
+    });
+}
+
+#[test]
+fn gen_test() {
+    let mut word_list = [""; 50];
+    (0..50).for_each(|i| {
+        let word = gen();
+        assert!(!word_list.contains(&word), 
+            "List of up to 50 previously generated words contains duplicates.");
+        assert!(!word.is_empty(), "generated word should not be empty");
+        word_list[i] = word;
+    })
+}
+
+#[test]
+fn gen_starts_with_test() {
+    ('a'..='z').chain('A'..='Z').for_each(|character| {
+        (0..5000).for_each(|_|{
+            let word = gen_starts_with(character).expect("character is not alphabetic");
+            let character = character.to_ascii_lowercase();
+            assert!(!word.is_empty(), "word should not be empty");
+            assert!(word.starts_with(character), "word should start with {character}");
+        })
+    })
+}
+
+
+#[test]
+fn gen_all_starts_with_test() {
+    ('a'..='z').chain('A'..'Z').for_each(|character| {
+        let word_list = gen_all_starts_with(character).expect("character is not alphabetic");
+        word_list.iter().for_each(|word| {
+            let character = character.to_ascii_lowercase();
+            assert!(!word.is_empty(), "word should not be empty");
+            assert!(word.starts_with(character), "word should start with {character}");
+        })
+    })
+}
+
+
+#[test]
+fn gen_all_test() {
+    let word_list = gen_all();
+    word_list.iter().for_each(|word| {
+        assert!(!word.is_empty(), 
+            "word should not be empty in word list");
+        assert!(word.chars().all(|c| ('a'..='z').contains(&c)),
+            "word should only contain lowercase alphabetical characters");
+    })
+}
+
+#[test]
+fn gen_all_indexes_from_first_char() {
+    let word_list = gen_all();
+    ('a'..='z').for_each(|c| {
+        let _ = word_list.iter().position(|w| w.starts_with(c))
+            .expect("words starting with any alphabetic character 
+                should be represented in words array");
+    });
+}
